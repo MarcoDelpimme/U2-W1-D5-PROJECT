7dada1dee5306099bd0e22875ab89049fa84bef5
MARCO DELPOPOLO <156794727+MarcoDelpimme@users.noreply.github.com>
2024-02-18 21:15:29 +0100

    Finish Work

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,11 +1,26 @@
-let svg = document.querySelector("#svg");
-let lettereM = svg.querySelectorAll("path");
+const navBar = document.querySelector("header");
+const buttonNavbar = document.querySelector("header button");
 
-let animazione = anime({
-  targets: lettereM,
-  opacity: [1, 0],
-  duration: 2000,
-  loop: true,
-  direction: "alternate",
-  easing: "easeInOutSine",
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 400) {
+    navBar.style.backgroundColor = "white";
+    buttonNavbar.style.backgroundColor = "green";
+    navBar.style.transition = "0.5s";
+    console.log(navBar);
+  } else {
+    navBar.style.backgroundColor = "#ffc017";
+    buttonNavbar.style.backgroundColor = "black";
+  }
 });
+
+let svgContainer = document.getElementById("svg");
+let svg = document.querySelector("svg");
+
+let gNode = svg.querySelectorAll("g[stroke-linecap]");
+
+function swappingM() {
+  let random = gNode[Math.floor(Math.random() * gNode.length)];
+  random.setAttribute("opacity", Math.floor(Math.random() * 2));
+}
+
+const intervalM = setInterval(swappingM, 10);
